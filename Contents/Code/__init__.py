e12d8df574cfdb59f0732fb544710ec384ab9da1
Fabien Piuzzi <reefab@demenzia.net>
2012-12-12 12:44:35 +0100

    Initial version

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-APPLICATIONS_PREFIX = "/applications/apptv"
+APPLICATIONS_PREFIX = "/applications/steamtv"
 
 NAME = L('Title')
 ART  = 'art-default.jpg'
@@ -8,73 +8,38 @@ ICON = 'icon-default.png'
 
 
 def Start():
-	Plugin.AddPrefixHandler(APPLICATIONS_PREFIX, ApplicationsMainMenu, NAME, ICON, ART)
+    Plugin.AddPrefixHandler(APPLICATIONS_PREFIX, ApplicationsMainMenu, NAME, ICON, ART)
 
-	Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
-	Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
+    Plugin.AddViewGroup("InfoList", viewMode="List", mediaType="items")
 
-	MediaContainer.title1 = NAME
-	MediaContainer.viewGroup = "List"
-	MediaContainer.art = R(ART)
-	DirectoryItem.thumb = R(ICON)
-	VideoItem.thumb = R(ICON)
-
-
-def GetBasePath():
-	return os.path.expanduser(os.path.expandvars(Prefs['basepath']))
-
-
-def ValidatePrefs():
-	if (os.path.isdir(GetBasePath())):
-		return MessageContainer(
-			L('Success'),
-			L('SettingsUpdated')
-		)
-	else:
-		return MessageContainer(
-			L('Error'),
-			L('InvalidBasepath')
-		)
+    MediaContainer.title1 = NAME
+    MediaContainer.viewGroup = "InfoList"
+    MediaContainer.art = R(ART)
+    DirectoryItem.thumb = R(ICON)
+    VideoItem.thumb = R(ICON)
 
 
 def ApplicationsMainMenu():
-	dir = MediaContainer(viewGroup="InfoList")
+    dir = MediaContainer(viewGroup="InfoList")
 
-	basePath = GetBasePath()
-	applications = os.listdir(basePath)
-
-	for application in applications:
-		name = os.path.basename(application)
-
-		# Only append non-hidden .apps or extension-less files (presumed aliases)
-		if (("." not in name) or (not name.startswith(".") and name.endswith(".app"))):
-			name = os.path.splitext(name)[0]
-
-			dir.Append(
-				Function(
-					DirectoryItem(
-						LaunchApplication,
-						name,
-						subtitle=L('LaunchSubtitle'),
-						summary=L('LaunchSummary') + name,
-						thumb=R(ICON),
-						art=R(ART)
-					),
-					path = basePath + "/" + application
-				)
-			)
-
-	dir.Append(
-		PrefsItem(
-			title=L('PreferencesTitle'),
-			subtile=L('PreferencesSubtitle'),
-			summary=L('PreferencesSummary'),
-			thumb=R(ICON)
-		)
-	)
-
-	return dir
+    dir.Append(
+        Function(
+             DirectoryItem(
+                 LaunchApplication,
+                 NAME,
+                 subtitle=L('LaunchSubtitle'),
+                 summary=L('LaunchSummary'),
+                 thumb=R(ICON),
+                 art=R(ART)
+             )
+         )
+     )
 
 
-def LaunchApplication(sender, path):
-	os.system("open " + path.replace(" ", "\\ "))
+
+    return dir
+
+
+def LaunchApplication(sender):
+    os.system("open steam://open/bigpicture")
+    return MessageContainer(NAME, L('WaitMessage'))
---
+++ README.md
@@ -0,0 +1,14 @@
+# SteamTV
+
+Launch Steam Big Picture Mode directly from Plex
+
+## Installation
+
+1. Rename repository folder or duplicate thereof to SteamPlex.bundle
+2. Copy to ~/Library/Application Support/Plex Media Server/Plugins
+3. Launch it from the Application menu item in Plex
+4. Wait a while for Big Picture Mode to launch, it's kinda slow to get started
+
+## Screenshot
+
+![screenshot](https://raw.github.com/reefab/SteamPlex/master/screenshot.png)
